4d6652c41b13ddb8a77aa0befae934932e9636bc
gavin <gav.estenssoro@gmail.com>
2018-09-26 23:01:54 -0600

    2.5 react setup

--- services/users/manage.py
+++ services/users/manage.py
@@ -39,7 +39,10 @@ def seed_db():
 @cli.command()
 def test():
     '''Runs the tests without code coverage'''
-    tests = unittest.TestLoader().discover('./project/tests', pattern='test*.py')
+    tests = unittest.TestLoader().discover(
+                './project/tests',
+                pattern='test*.py'
+                )
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
         return 0
