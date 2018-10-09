9f7e493d82d0deab75940e7df06355aac21b3cf7
gavin <gav.estenssoro@gmail.com>
2018-10-08 21:53:10 -0600

    4.2 end-to-end test setup

---
+++ test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+fails=""
+
+inspect() {
+    if [ $1 -ne 0 ]; then
+        fails="${fails} $2"
+    fi
+}
+
+# run unit and integration tests
+docker-compose -f docker-compose-dev.yml up -d --build
+docker-compose -f docker-compose-dev.yml run users python manage.py test
+inspect $? users
+docker-compose -f docker-compose-dev.yml run users flake8 project
+inspect $? users-lint
+docker-compose -f docker-compose-dev.yml run client npm test -- --coverage
+inspect $? client
+docker-compose -f docker-compose-dev.yml down
+
+# run e2e tests
+docker-compose -f docker-compose-prod.yml up -d --build
+docker-compose -f docker-compose-prod.yml run users python manage.py recreate_db
+./node_modules/.bin/cypress run --config baseUrl=http://localhost
+inspect $? e2e
+docker-compose -f docker-compose-prod.yml up -d --build
+
+# return proper code
+if [ -n "${fails}" ]; then
+    echo "Tests failed: ${fails}"
+    exit 1
+else
+    echo "Tests passed!"
+    exit 0
+fi
